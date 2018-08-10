4756314e034062711f6f2489e4b9025b8d5867e8
nobelium27 <shin10912@gmail.com>
2018-08-09 23:34:44 -0500

    mute works!

--- Bento/Modules/tb.cs
+++ Bento/Modules/tb.cs
@@ -16,63 +16,86 @@ namespace Bento.Modules
 
         private static Random random = new Random();
         //time set for the user to answer
-        private static int time = random.Next(15, 40);
+        private static int time = 0;
         //sets wires used in the bomb
-        private string[] randomWires = RandomWires();
+        private static string[] randomWires = new string[] { "Poop" };
         //contains the correct wire value
         private static int correct;
         //default user 
-        private static IGuildUser user;
+        private static SocketUser user;
         private static Timer muteTime;
         private static IGuild currentGuild;
+        private static Timer bombTime;
 
 
         [Command("tb")]
         public async Task BombSet()
         {
-            user = Context.Guild.CurrentUser;
+            user = Context.Message.Author;
             currentGuild = Context.Guild;
+            time = random.Next(15, 40);
+            randomWires = RandomWires();
             string wireString = String.Join(", ", randomWires);
             EmbedBuilder embed = new EmbedBuilder();
             embed.AddField($"Bomb has been planted",
-                $"Bento bot stuffs the bomb into {user}'s pants.  The display reads {time} seconds." +
+                $"Bento bot stuffs the bomb into {user.Mention}'s pants.  The display reads {time} seconds." +
                 $"Diffuse the bomb by cutting the correct wire.There are {randomWires.Length} wires.They are {wireString}.");
 
             embed.WithColor(Color.Red);
+            Console.WriteLine(randomWires[correct]);
+            Console.WriteLine("bomb Timer setting");
 
-            Timer bombTime = new Timer(time * 1000);
-
-
+            //bomb timer start
+            bombTime = new Timer(time * 1000);
+            bombTime.Start();
+            //bomb timer hits 0
+            bombTime.Elapsed += bombTimeHandle;
             await ReplyAsync("", false, embed.Build());
         }
+
+        
         [Command("tb")]
-        public async Task BombSet(IGuildUser user1)
+        public async Task BombSet(SocketGuildUser user1)
         {
             user = user1;
             currentGuild = Context.Guild;
+            time = random.Next(15, 40);
+            randomWires = RandomWires();
             string wireString = String.Join(", ", randomWires);
             EmbedBuilder embed = new EmbedBuilder();
             embed.AddField($"Bomb has been planted",
-                $"Bento bot stuffs the bomb into {user}'s pants.  The display reads {time} seconds." +
+                $"Bento bot stuffs the bomb into {user.Mention}'s pants.  The display reads {time} seconds." +
                 $"Diffuse the bomb by cutting the correct wire.There are {randomWires.Length} wires.They are {wireString}.");
 
             embed.WithColor(Color.Red);
+            bombTime = new Timer(time * 1000);
+            Console.WriteLine("bomb start?");
+            bombTime.Start();
+            bombTime.Elapsed += bombTimeHandle;
+
 
             await ReplyAsync("", false, embed.Build());
         }
         //does the same thing as above but allows additional comments after the mention
         [Command("tb")]
-        public async Task BombSet(IGuildUser user1, [Remainder] string remainder)
+        public async Task BombSet(SocketGuildUser user1, [Remainder] string remainder)
         {
             user = user1;
             currentGuild = Context.Guild;
+            time = random.Next(15, 40);
+            randomWires = RandomWires();
             string wireString = String.Join(", ", randomWires);
+
             EmbedBuilder embed = new EmbedBuilder();
             embed.AddField($"Bomb has been planted",
-                $"Bento bot stuffs the bomb into {user}'s pants.  The display reads {time} seconds." +
+                $"Bento bot stuffs the bomb into {user.Mention}'s pants.  The display reads {time} seconds." +
                 $"Diffuse the bomb by cutting the correct wire.There are {randomWires.Length} wires.They are {wireString}.");
 
             embed.WithColor(Color.Red);
+            bombTime = new Timer(time * 1000);
+            Console.WriteLine("bomb start?");
+            bombTime.Start();
+            bombTime.Elapsed += bombTimeHandle;
 
             await ReplyAsync("", false, embed.Build());
         }
@@ -83,18 +106,28 @@ namespace Bento.Modules
         {
             //true if answer was correct
             bool correctAnswer = false;
+            Console.WriteLine(correct);
+            
+            Console.WriteLine(randomWires[0]);
+            Console.WriteLine(randomWires[correct]);
+            Console.WriteLine(answer);
+
             if (answer == null)
             {
                 Console.WriteLine("Select a color or its number");
                 return;
             }
-            else if (answer == randomWires[correct] || Convert.ToInt32(answer) - 1 == correct)
+            else if (answer.Equals(randomWires[correct]) || Convert.ToInt32(answer) - 1 == correct)
             {
+                Console.WriteLine("correct");
                 correctAnswer = true;
+                
             }
 
             if (correctAnswer){
-                Console.WriteLine("Bomb has been diffused");
+
+                bombTime.Stop();
+                await ReplyAsync("Bomb has been diffused");
                 return;
             }
             else
@@ -102,35 +135,59 @@ namespace Bento.Modules
                 BombGoesOff(currentGuild, user);
                
             }
-
-
+        }
+        private void bombTimeHandle(object sender, ElapsedEventArgs e)
+        {
+            
+            Console.WriteLine("bomeTimeHandle");
+            BombGoesOff(currentGuild, user);
+            bombTime.Stop();
         }
 
 
         public async void BombGoesOff(IGuild guild, IUser user)
         {
+            Console.WriteLine("bombGoesOff");
             int muteSec = random.Next(30, 120);
             Mute(muteSec, currentGuild, user);
-            await ReplyAsync($"{user} has been muted for {muteSec} seconds");
+            await ReplyAsync($"**BOOOOOOOOM** {user} has been muted for {muteSec} seconds");
         }
 
         //mute user
         public async void Mute(int muteSec, IGuild guild, IUser user)
         {
             //MUTE USER HERE
-
-            await (user as IGuildUser).ModifyAsync(x => x.Mute = true);
+            var role1 = Context.Guild.Roles;
+            var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == "Muted");
+            var roleEvery = Context.Guild.Roles.FirstOrDefault(x => x.Name == "@everyone");
+            await (user as IGuildUser).AddRoleAsync(role);
+            //await (user as IGuildUser).RemoveRoleAsync(roleEvery);
+            var botPermission = Context.Client;
             
-            Timer muteTime = new Timer(muteSec * 1000);
+
+            //var perma = (user as IGuildUser).AddRoleAsync(role);
+
+            //await (user as IGuildUser).ModifyAsync(x => x.Mute = true);
+            Console.WriteLine("role?");     
+            //start timer for the length of the mute
+            muteTime = new Timer(muteSec * 1000);
+            muteTime.Start();
             muteTime.Elapsed += HandleTimerElapsed;
 
+            
             
         }
         //unmute
         public async void HandleTimerElapsed(object sender, ElapsedEventArgs e)
         {
+            muteTime.Stop();
             // UNMUTE USER
-            await(user as IGuildUser).ModifyAsync(x => x.Mute = false);
+            var roleEvery = Context.Guild.Roles.FirstOrDefault(x => x.Name == "@everyone");
+            //await (user as IGuildUser).AddRoleAsync(roleEvery);
+            var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == "Muted");
+            await (user as IGuildUser).RemoveRoleAsync(role);
+            
+
         }
 
         //sets existing wires and return in sorted order
@@ -173,6 +230,7 @@ namespace Bento.Modules
             Array.Sort(randomWires);
             return randomWires;
         }
+
 
         
 
